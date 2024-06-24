e6f7ac774e808f909c1f4ad160349f7fb57081b2
Teregan <luis.rojas.ing@gmail.com>
2024-06-23 20:07:27 -0400

    instalación de cap, se agrego la carpeta android y el uso de la cámara

--- src/app/control/control.page.ts
+++ src/app/control/control.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-control',
@@ -10,6 +11,26 @@ export class ControlPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    Camera.requestPermissions();
+  }
+  takePhoto(){
+    const takePicture = async () => {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera
+      });
+    
+      // image.webPath will contain a path that can be set as an image src.
+      // You can access the original file using image.path, which can be
+      // passed to the Filesystem API to read the raw data of the image,
+      // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+      var imageUrl = image.webPath;
+    
+      // Can be set to the src of an image now
+      //imageElement.src = imageUrl;
+    };
   }
 
 }
